2006381adae0e6376c2bba1b182caaedffabfb8d
saqibs98 <saqibs186@gmail.com>
2021-04-09 12:03:57 +0500

    1.0.4

--- controller/requestController.js
+++ controller/requestController.js
@@ -1,10 +1,7 @@
 const axios = require('axios')
 const qs = require('querystring');
 module.exports={
-
-//flag identifies which configuration to be sent as stringified 
-
-httpRequest : (config,flag)  => {
+httpRequest : (config,flag)  => {   //flag identifies which configuration to be sent as stringified 
     var localConfig =  {...config};
     if(flag){
         localConfig.data=qs.stringify(localConfig.data);
